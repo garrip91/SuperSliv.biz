f974582d89648f3c85e9f3835746bfa69ef2b533
garrip91 <garrip91@gmail.com>
2023-03-22 19:27:59 +0300

    Create class_SMSSender.py

--- Lesson_8.2.5/class_Fish.py
+++ Lesson_8.2.5/class_Fish.py
@@ -1,1 +1,20 @@
-#
+class Fish():
+
+    def swim(self):
+        print("Я плаваю!")
+
+
+class Girl():
+    
+    def sing(self):
+        print("Я пою!")
+
+
+class Mermaid(Girl, Fish):
+
+    pass
+
+
+ariel = Mermaid()
+ariel.swim()
+ariel.sing()
