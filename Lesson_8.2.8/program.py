284c6fe9d7e335f33dd35ec2f4d3d647bc6804ef
garrip91 <garrip91@gmail.com>
2023-03-22 22:16:47 +0300

    Update program.py

--- Lesson_8.2.8/program.py
+++ Lesson_8.2.8/program.py
@@ -1,6 +1,9 @@
 class Question:
 
     def __init__(self, question_text, question_level, true_answer, question_asked=False, user_answer=None, scores=0):
+        """
+        Наш инициализатор.
+        """
         self.question_text = question_text
         self.question_level = question_level
         self.true_answer = true_answer
@@ -15,10 +18,37 @@ class Question:
         """
         return int(self.question_level.split("/")[0]) * 10
     
+    def is_correct(self) -> bool:
+        """
+        Возвращает True, если ответ пользователя совпадает с верным ответом, иначе False.
+        """
+        return True if self.user_answer==self.true_answer else False
+    
+    def build_question(self) -> str:
+        """
+        Возвращает вопрос в понятном пользователю виде, например:
+        Вопрос: What do people often call American flag?
+        (то есть, возвращает строковый тип данных)
+        Сложность 4/5.
+        """
+        return self.question_text
+    
+    def build_positive_or_negative_feedback(self) -> str:
+        """
+        Возвращает: Ответ верный, получено __ баллов! - в случае верного ответа;
+        Ответ неверный, верный ответ: __ ! - в случае неверного ответа.
+        """
+        if self.is_correct():
+            return f"Ответ верный, получено {self.scores} баллов!"
+        else:
+            return f"Ответ неверный, верный ответ: {self.true_answer} !"
+    
     def __str__(self):
-        """Метод для проверки класса"""
-        return f"{self.question_text}\n{self.question_level}\n{self.true_answer}\n{self.question_asked}\n{self.user_answer}\n{self.scores}\n{self.get_points()}"
+        """
+        Метод для проверки класса.
+        """
+        return f"{self.question_text}\n{self.question_level}\n{self.true_answer}\n{self.question_asked}\n{self.user_answer}\n{self.scores}\n{self.get_points()}\n{self.is_correct()}\n{self.build_question()}\n{self.build_positive_or_negative_feedback()}"
 
 
-question = Question(question_text="What do people often call American flag?", question_level="4/5", true_answer="stars and stripes")
+question = Question(question_text="What do people often call American flag?", question_level="4/5", true_answer="stars and stripes", question_asked=False, user_answer="!stars and stripes", scores=0)
 print(question)
