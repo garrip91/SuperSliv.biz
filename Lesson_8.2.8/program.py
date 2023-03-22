e363b11480d558bf3b6fe300496c5e25c7b187ff
garrip91 <garrip91@gmail.com>
2023-03-22 21:34:03 +0300

    Update program.py

--- Lesson_8.2.8/program.py
+++ Lesson_8.2.8/program.py
@@ -8,9 +8,17 @@ class Question:
         self.user_answer = user_answer
         self.scores = scores
     
+    def get_points(self):
+        """
+        Возвращает int, количество баллов.
+        Баллы зависят от сложности: за 1 даётся 10 баллов, за 5 даётся 50 баллов.
+        """
+        return int(self.question_level.split("/")[0]) * 10
+    
     def __str__(self):
-        return f"{self.question_text}, {self.question_level}, {self.true_answer}, {self.question_asked}, {self.user_answer}, {self.scores}"
+        """Метод для проверки класса"""
+        return f"{self.question_text}\n{self.question_level}\n{self.true_answer}\n{self.question_asked}\n{self.user_answer}\n{self.scores}\n{self.get_points()}"
 
 
-question = Question("Текст вопроса", "Уровень сложности", "Верный ответ")
+question = Question(question_text="What do people often call American flag?", question_level="4/5", true_answer="stars and stripes")
 print(question)
