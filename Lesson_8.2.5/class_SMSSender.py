3e90c774fb6c06a397a9a7538a0700e4948224c6
garrip91 <garrip91@gmail.com>
2023-03-22 20:26:28 +0300

    Update class_SMSSender.py

--- Lesson_8.2.5/class_SMSSender.py
+++ Lesson_8.2.5/class_SMSSender.py
@@ -1,1 +1,16 @@
-#
+class SMSSender():
+
+    def send_sms(self, message):
+        print("Отправим сообщение через смс:", message)
+
+
+class PushSender():
+
+    def send_push(self, message):
+        print("Отправим сообщение через пуш-уведомления:", message)
+
+
+class MailSender():
+
+    def send_mail(self, message):
+        print("Отправим сообщение через почту:", message)
