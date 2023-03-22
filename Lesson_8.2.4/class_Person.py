608c145063cde1092b7996d6294a8a4b370a9d66
garrip91 <garrip91@gmail.com>
2023-03-22 19:15:42 +0300

    Update class_Person.py

--- Lesson_8.2.4/class_Person.py
+++ Lesson_8.2.4/class_Person.py
@@ -1,1 +1,28 @@
-#
+class Person():
+
+    def eat(self):
+        print("Ням-ням!")
+
+
+class Employee(Person):
+
+    def visit_demo(self):
+        print("Все сотрудники ходят на демо. Я тоже схожу!")
+
+
+class Developer(Employee):
+
+    def review_code(self):
+        print("Делаю код-ревью коллеге. Всё неплохо, пара замечаний и мержим!")
+
+
+class FrontendDeveloper(Developer):
+
+    def code_frontend(self):
+        print("Делаю запрос на сервер, кидаю данные в стор, раскладываю по компонентам и пушим!")
+
+
+class BackendDeveloper(Developer):
+
+    def code_backend(self):
+        print("Ловим параметры запроса, проверяем права, дёргаем БД, сериализуем, пишем тест, тестим и пушим!")
