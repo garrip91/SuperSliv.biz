f464692db3447ec2684b64cfc553c270dab4c0f5
garrip91 <garrip91@gmail.com>
2023-03-22 20:34:50 +0300

    Update class_SMSSender.py

--- Lesson_8.2.5/class_SMSSender.py
+++ Lesson_8.2.5/class_SMSSender.py
@@ -14,3 +14,15 @@ class MailSender():
 
     def send_mail(self, message):
         print("Отправим сообщение через почту:", message)
+
+
+class SuperSender(SMSSender, PushSender, MailSender):
+
+    def send_all(self, message):
+        self.send_sms(message)
+        self.send_push(message)
+        self.send_mail(message)
+
+
+sender = SuperSender()
+sender.send_all("Text message")
