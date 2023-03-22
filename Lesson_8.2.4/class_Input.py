f57e30c06051134e5d35a9ec270e7eabb59a9eb0
garrip91 <garrip91@gmail.com>
2023-03-22 18:55:44 +0300

    Update class_Input.py

--- Lesson_8.2.4/class_Input.py
+++ Lesson_8.2.4/class_Input.py
@@ -1,1 +1,33 @@
-#
+class Input():
+
+    def __init__(self, name, value=None):
+        self.name = name
+        self.value = value
+
+
+class TextInput(Input):
+
+    def is_empty(self):
+        return self.value == None
+
+
+class EmailInput(TextInput):
+
+    def check(self):
+        has_at = self.value.count("@") == 1
+        has_dot = self.value.count(".") >= 1
+        if has_at and has_dot:
+            return True
+        return False
+
+
+class PasswordInput(TextInput):
+
+    def check(self):
+        if len(self.value) < 8:
+            return False
+
+
+usermail = EmailInput("usermail", "me@sky.pro")
+print(usermail.is_empty())
+print(usermail.check())
