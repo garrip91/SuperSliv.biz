a71e2ee59fe34821e7413dd51ea59c6ebbe42bdb
garrip91 <garrip91@gmail.com>
2023-03-23 22:01:03 +0300

    Update question.py

---
+++ Lesson_8.4/question.py
@@ -0,0 +1,19 @@
+class Question:
+
+    def __init__(self):
+        pass
+
+    def get_points(self):
+        pass
+
+    def is_correct(self):
+        pass
+
+    def build_question(self):
+        pass
+
+    def build_positive_feedback(self):
+        pass
+
+    def build_negative_feedback(self):
+        pass
