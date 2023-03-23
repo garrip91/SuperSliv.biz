388f8458812ede95ffb64e6413fc9ff9c56d7b2c
garrip91 <garrip91@gmail.com>
2023-03-23 22:09:53 +0300

    Update question.py

--- Lesson_8.4/question.py
+++ Lesson_8.4/question.py
@@ -1,7 +1,12 @@
 class Question:
 
     def __init__(self, question_text, question_level, question_answer):
-        pass
+        self.question_text = question_text
+        self.question_level = question_level
+        self.question_answer = question_answer
+        self.is_asked = False
+        self.user_answer = None
+        self.points = self.question_level * 10
 
     def get_points(self):
         pass
