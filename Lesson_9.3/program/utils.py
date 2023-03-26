8a0a5c831436e564a0933012edcc4a76fe988dd7
garrip91 <garrip91@gmail.com>
2023-03-26 13:51:22 +0300

    Update utils.py

--- Lesson_9.3/program/utils.py
+++ Lesson_9.3/program/utils.py
@@ -1,3 +1,12 @@
 class BasicWord:
 
-    pass
+    def __init__(self, original_word, valid_words):
+        self.original_word = original_word
+        self.valid_words = valid_words
+    
+    def is_in_json_list(self) -> bool:
+        with pass
+        pass
+
+    def subwords_count(self):
+        pass
