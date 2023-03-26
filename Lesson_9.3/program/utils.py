d1be950e2f7bf8283df11c082cbcaa55c5dffff7
garrip91 <garrip91@gmail.com>
2023-03-26 15:04:17 +0300

    Update utils.py

--- Lesson_9.3/program/utils.py
+++ Lesson_9.3/program/utils.py
@@ -1,12 +1,24 @@
+import json
+
+
 class BasicWord:
 
     def __init__(self, original_word, valid_words):
         self.original_word = original_word
         self.valid_words = valid_words
     
-    def is_in_json_list(self) -> bool:
-        with pass
-        pass
+    def is_word_in_json_list(self) -> bool:
+        with open(file="words.json", mode="r", encoding="utf-8") as file:
+            words_and_subwords = json.load(file)
+            for word in words_and_subwords:
+                if self.original_word in list(word.items())[0]:
+                    return True
+                else:
+                    return False
 
     def subwords_count(self):
         pass
+
+
+basic_word = BasicWord("питон", ["пони", "тон", "ион", "опт", "пот", "тип", "топ", "пион", "понт"])
+print(basic_word.is_word_in_json_list())
