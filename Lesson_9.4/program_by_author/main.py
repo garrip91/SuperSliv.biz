39eb32653186ec19268ea65ecc5b77ed1ad440e3
garrip91 <garrip91@gmail.com>
2023-03-28 17:51:45 +0300

    Update main.py

--- Lesson_9.4/program_by_author/main.py
+++ Lesson_9.4/program_by_author/main.py
@@ -4,14 +4,16 @@ from utils import load_random_word
 def main():
     
     main_word = load_random_word()
-    
-    print("Введите слово")
-    user_input = input()
 
-    if main_word.has_subword(user_input):
-        print("Слово есть!")
-    else:
-        print("Слова нет!")
+    while game_is_on:
+    
+        print("Введите слово")
+        user_input = input()
+
+        if main_word.has_subword(user_input):
+            print("Слово есть!")
+        else:
+            print("Слова нет!")
 
 
 if __name__ == "__main__":
