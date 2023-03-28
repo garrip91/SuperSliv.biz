097164ca8fe5c0415843c3b5ffbd95ee5c0feb3c
garrip91 <garrip91@gmail.com>
2023-03-28 17:42:36 +0300

    Update basic_word.py

--- Lesson_9.4/program_by_author/basic_word.py
+++ Lesson_9.4/program_by_author/basic_word.py
@@ -4,8 +4,8 @@ class BasicWord:
         self.word = word
         self.sub_words = sub_words
     
-    def has_subword(self, candidate): # ДОРАБОТАТЬ
-        return candidate.lower()      # МЕТОД
+    def has_subword(self, candidate):              # ДОРАБОТАТЬ
+        return candidate.lower() in self.sub_words # МЕТОД
     
     def count_subwords(self):         # ДОРАБОТАТЬ
         return 7                      # МЕТОД
