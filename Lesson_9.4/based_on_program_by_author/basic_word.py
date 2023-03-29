3bc2af35ec36de777641fe936e5384a4c68233ad
garrip91 <garrip91@gmail.com>
2023-03-29 19:48:34 +0300

    Any fixes

--- Lesson_9.4/based_on_program_by_author/basic_word.py
+++ Lesson_9.4/based_on_program_by_author/basic_word.py
@@ -2,13 +2,13 @@ class BasicWord:
 
     def __init__(self, word, sub_words):
         self.word = word
-        self.sub_words = sub_words
+        self.sub_words = sub_words.split(", ")
     
-    def has_subwords(self, candidate):              # ДОРАБОТАТЬ
+    def has_subword(self, candidate):              # ДОРАБОТАТЬ
         return candidate.lower() in self.sub_words # МЕТОД
     
     def subwords_count(self):         # ДОРАБОТАТЬ
-        return len(self.sub_words.split(", "))                      # МЕТОД
+        return len(self.sub_words)                      # МЕТОД
     
     #def __repr__(self):
     #    return f"Составьте {self.subwords_count()} слов из слова {self.word.upper()}"
@@ -18,4 +18,4 @@ class BasicWord:
 
 
 #basic_word = BasicWord("питон", "пони, тон, ион, опт, пот, тип, топ, пион, понт")
-#print(basic_word)
+#print(basic_word.has_subword("123"))
--- Lesson_9.4/based_on_program_by_author/main.py
+++ Lesson_9.4/based_on_program_by_author/main.py
@@ -6,28 +6,26 @@ def main():
     
     flag = True
 
-    word_and_subwords_tuple = load_random_word()
-    subwords_count = load_random_word().subwords_count()
-    word = load_random_word().word
-    print(subwords_count)
-    print(word)
-    #subwords_count = word_and_subwords_tuple.subwords_count()
-
-    #print(f"Составьте {wordcount} слов из слова {word}")
-
     while flag:
+
+        word_and_subwords = load_random_word()
+        subwords_count = word_and_subwords.subwords_count()
+        word = word_and_subwords.word
     
         username = input("Введите Ваше имя:\n")
         player = Player(username)
         print(f"Здравствуйте, {player.name}!")
         print(f"Составьте {subwords_count} слов из слова {word.upper()}")
+        
+        print(f"ПОДСКАЗКА: {word_and_subwords.sub_words}")
+
         print("Слова должны быть не короче 3 букв")
         print('Чтобы закончить игру, угадайте все слова или введите слово "stop" и нажмите на "Enter"')
         
-        print("Введите слово")
+        print("Поехали! Ваше первое слово:\n")
         user_input = input()
 
-        if word_and_subwords_tuple.has_subwords(user_input):
+        if word_and_subwords.has_subword(user_input):
             print("Слово есть!")
         else:
             print("Слова нет!")
