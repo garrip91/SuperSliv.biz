1c82d0b906ea57337ba056551258d523a11aa013
garrip91 <garrip91@gmail.com>
2023-03-29 14:40:34 +0300

    Update create_and_fill_db.py

--- Lesson_9.4/based_on_program_by_author/create_and_fill_db.py
+++ Lesson_9.4/based_on_program_by_author/create_and_fill_db.py
@@ -17,8 +17,8 @@ cursor.execute(
 conn.commit()
 
 # Добавляем в БД нужные нам записи:
-cursor.execute(f"INSERT INTO 'words_and_subwords' (word, sub_word) VALUES (?, ?)", ["питон", "['пони', 'тон', 'ион', 'опт', 'пот', 'тип', 'топ', 'пион', 'понт']"])
-cursor.execute(f"INSERT INTO 'words_and_subwords' (word, sub_word) VALUES (?, ?)", ["набор", "['бар', 'бон', 'бор', 'раб', 'бра', 'боа', 'нора', 'роба', 'барон']"])
-cursor.execute(f"INSERT INTO 'words_and_subwords' (word, sub_word) VALUES (?, ?)", ["строка", "['акр', 'акт', 'кот', 'рак', 'орк', 'оса', 'сок', 'ток', 'тор', 'кора', 'коса', 'сота', 'торс', 'роса', 'скат']"])
+cursor.execute(f"INSERT INTO 'words_and_subwords' (word, sub_word) VALUES (?, ?)", ["питон", "пони, тон, ион, опт, пот, тип, топ, пион, понт"])
+cursor.execute(f"INSERT INTO 'words_and_subwords' (word, sub_word) VALUES (?, ?)", ["набор", "бар, бон, бор, раб, бра, боа, нора, роба, барон"])
+cursor.execute(f"INSERT INTO 'words_and_subwords' (word, sub_word) VALUES (?, ?)", ["строка", "акр, акт, кот, рак, орк, оса, сок, ток, тор, кора, коса, сота, торс, роса, скат"])
 # Снова записываем изменения:
 conn.commit()
