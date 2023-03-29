e691b329feb2526f2516c85e2ca835da9d96884d
garrip91 <garrip91@gmail.com>
2023-03-29 19:58:30 +0300

    Update main.py

--- Lesson_9.4/based_on_program_by_author/main.py
+++ Lesson_9.4/based_on_program_by_author/main.py
@@ -20,15 +20,18 @@ def main():
         print(f"ПОДСКАЗКА: {word_and_subwords.sub_words}")
 
         print("Слова должны быть не короче 3 букв")
-        print('Чтобы закончить игру, угадайте все слова или введите слово "stop" и нажмите на "Enter"')
+        print('Чтобы закончить игру, угадайте все слова или В ПРОЦЕССЕ ИГРЫ введите слово "stop" и нажмите на "Enter"')
         
         print("Поехали! Ваше первое слово:\n")
         user_input = input()
 
-        if word_and_subwords.has_subword(user_input):
-            print("Слово есть!")
+        if user_input == "stop":
+            break
         else:
-            print("Слова нет!")
+            if word_and_subwords.has_subword(user_input):
+                print("Слово есть!")
+            else:
+                print("Слова нет!")
 
 
 if __name__ == "__main__":
