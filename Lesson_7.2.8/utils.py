754d18f28b27f87577db91a766e0c1ec5e8d7be0
garrip91 <garrip91@gmail.com>
2023-03-30 16:06:02 +0300

    Update utils.py

--- Lesson_7.2.8/utils.py
+++ Lesson_7.2.8/utils.py
@@ -1,12 +1,6 @@
 import json
 
 
-#def load_questions():
-#    """Загружает вопросы из файла"""
-
-#    with open(file="questions.json", mode="r", encoding="utf-8") as fp:
-#        questions_dict = json.load(fp)
-#        return questions_dict
 def load_questions_from_json():
     """Загружает вопросы из файла"""
 
@@ -15,14 +9,6 @@ def load_questions_from_json():
         return data
 
 
-#def show_field():
-#    """Выводит игровое поле"""
-
-#    table = ""
-#    for k, v in load_questions().items():
-#        score_str_with_spaces = "   ".join([i for i in v.keys()])
-#        table += f"{k}   {score_str_with_spaces}\n"
-#    return table
 def show_field(questions):
     """Выводит игровое поле"""
 
@@ -35,14 +21,8 @@ def show_field(questions):
             else:
                 print("   ".ljust(5), end=" ")
         print()
-#questions = load_questions_from_json()
-#show_field(questions)
 
 
-#def parse_input(string):
-#    """Делит ввод пользователя на категорию и число"""
-
-#    return string.split(" ")
 def parse_input(user_input):
     """Делит ввод пользователя на категорию и число"""
 
@@ -54,16 +34,8 @@ def parse_input(user_input):
     if not user_data[1].isdigit():
         return False
     return {"category": user_data[0], "price": user_data[1]}
-#print(parse_input("Животные 100"))
 
 
-#def show_question(parse_input_data):
-#    """Печатает вопрос"""
-
-#    selected = load_questions()[parse_input_data[0]][parse_input_data[1]]
-#    question = f"Слово {selected['question']} в переводе означает ..."
-
-#    return selected, question
 def print_question(question_text):
     """Печатает вопрос"""
 
