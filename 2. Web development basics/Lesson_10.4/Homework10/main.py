021dbe3ee2ea362b0e6146cf0c627410a08bb785
garrip91 <garrip91@gmail.com>
2023-04-01 00:29:40 +0300

    Step by step

--- 2. Web development basics/Lesson_10.4/Homework10/main.py
+++ 2. Web development basics/Lesson_10.4/Homework10/main.py
@@ -1,14 +1,27 @@
 from flask import Flask
+from utils import load_candidates
 
 
 app = Flask(__name__)
 
-# @app.route("/")
+candidates = load_candidates()
+print(candidates)
+
+
+@app.route("/")
 def page_index():
-    return "Hello, World!"
+    str_candidates = "<pre>"
+    for candidate in candidates.values():
+        str_candidates += f"{candidate['name']} \n{candidate['position']} \n{candidate['skills']} \n\n"
+    str_candidates += "</pre>"
+    return str_candidates
 
 
-app.add_url_rule("/", view_func=page_index)
+@app.route("/candidates/<int:id>/")
+def profile(id):
+    candidate = candidates[id]
+    str_candidates = f"<img src={candidate['picture']} /> <br /><br />{candidate['name']} <br />{candidate['position']} <br />{candidate['skills']} <br /><br />"
+    return str_candidates
 
 
-app.run()
+app.run(debug=True)
---
+++ 2. Web development basics/Lesson_10.4/Homework10/utils.py
@@ -0,0 +1,14 @@
+import json
+from pprint import pprint
+
+
+def load_candidates():
+    with open(file="candidates.json", mode="r", encoding="utf-8") as file:
+        data = json.load(file)
+        candidates = {}
+        for i in data:
+            candidates[i["id"]] = i
+    return candidates
+
+
+#print(load_candidates())
