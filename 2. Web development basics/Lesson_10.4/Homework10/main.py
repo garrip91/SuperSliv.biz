d5e198e12b629754bab76e63e565ffc1300dd6ba
garrip91 <garrip91@gmail.com>
2023-03-31 22:21:20 +0300

    Fill main.py file

--- 2. Web development basics/Lesson_10.4/Homework10/main.py
+++ 2. Web development basics/Lesson_10.4/Homework10/main.py
@@ -1,1 +1,14 @@
-#
+from flask import Flask
+
+
+app = Flask(__name__)
+
+# @app.route("/")
+def page_index():
+    return "Hello, World!"
+
+
+app.add_url_rule("/", view_func=page_index)
+
+
+app.run()
