66e32074f96842ace13c52362391f345bc527a3a
garrip91 <garrip91@gmail.com>
2023-04-01 16:30:50 +0300

    Add view for find candidates by skills

--- 2. Web development basics/Lesson_10.4/Homework10/main.py
+++ 2. Web development basics/Lesson_10.4/Homework10/main.py
@@ -5,7 +5,7 @@ from utils import load_candidates
 app = Flask(__name__)
 
 candidates = load_candidates()
-print(candidates)
+#print(candidates)
 
 
 @app.route("/")
@@ -18,9 +18,21 @@ def page_index():
 
 
 @app.route("/candidates/<int:id>/")
-def profile(id):
+def page_profile(id):
     candidate = candidates[id]
     str_candidates = f"<img src={candidate['picture']} /> <br /><br />{candidate['name']} <br />{candidate['position']} <br />{candidate['skills']} <br /><br />"
+    return str_candidates
+
+
+@app.route("/skills/<skill>/")
+def page_skill(skill):
+    str_candidates = "<pre>"
+    for candidate in candidates.values():
+        candidate_skills = candidate["skills"].split(", ")
+        candidate_skills = [x.lower() for x in candidate_skills]
+        if skill.lower() in candidate_skills:
+            str_candidates += f"{candidate['name']} \n{candidate['position']} \n{candidate['skills']} \n\n"
+    str_candidates += "</pre>"
     return str_candidates
 
 
