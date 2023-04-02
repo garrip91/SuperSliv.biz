23225c1945ec6fc8a878560018f96e901c537d00
garrip91 <garrip91@gmail.com>
2023-04-02 15:25:36 +0300

    Add Lesson_11.1

---
+++ 2. Web development basics/Lesson_11.1/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, render_template
+
+
+app = Flask(__name__)
+
+#@app.route("/")
+#def page_index():
+#    page_content = """
+#        <h1>Смотри, это - моя страничка</h1>
+#        <p>Страничек много, но это - моя!</p>
+#        <p>Фронтенд - мой лучший друг!</p>
+#    """
+#    return page_content
+
+
+@app.route("/")
+def hello():
+    user_data = {
+        "name": "Ivan",
+        "phone": "+7 (123) 456-78-90",
+        "email": "ivan_dev@gmail.com",
+        "telegram": "ivan_dev"
+    }
+    return render_template("hello.html", user=user_data)
+
+
+app.run()
