e4af20d32566b1b254e466eb0fbbad7baff99836
garrip91 <garrip91@gmail.com>
2023-05-12 20:21:14 +0300

    Step 7

--- 2. Web development basics/Lesson_12.3/homework/lesson12_project_source_v3/loader/loader.py
+++ 2. Web development basics/Lesson_12.3/homework/lesson12_project_source_v3/loader/loader.py
@@ -1,5 +1,9 @@
 from flask import Blueprint, request, render_template
 from functions import load_posts, uploads_posts
+import logging
+
+
+logging.basicConfig(encoding="utf-8", level=logging.INFO)
 
 
 loader_blueprint = Blueprint("loader", __name__, url_prefix="/post", static_folder="static", template_folder="templates")
@@ -22,7 +26,10 @@ def upload():
         })
         uploads_posts(posts)
         file.save(f"uploads/images/{filename}")
+        if filename.split(".")[-1] not in ["png", "jpeg", "jpg"]:
+            logging.info("Файл не имеет нужного расширения!")
     except FileNotFoundError:
+        logging.error("Ошибка при загрузке файла!")
         return '<h1>Файл не найден</h1>'
     else:
         return render_template("post_uploaded.html", pic=f"/uploads/images/{filename}", content=content)
