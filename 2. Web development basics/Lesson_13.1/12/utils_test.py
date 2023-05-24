a3c03ea9c84913cdd63cc1975eafc83b9a9425e2
garrip91 <garrip91@gmail.com>
2023-05-24 21:07:46 +0300

    Using conftest.py

---
+++ 2. Web development basics/Lesson_13.1/12/utils_test.py
@@ -0,0 +1,8 @@
+import conftest
+from utils import sum_func
+
+
+def test_sum_func(two_numbers_sum):
+
+    sum_result = sum_func(two_numbers_sum[0], two_numbers_sum[1])
+    assert sum_result == two_numbers_sum[2]
