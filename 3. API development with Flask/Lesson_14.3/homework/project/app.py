a876f1e6b56d9d06a97239371cde82be58bb23f9
garrip91 <garrip91@gmail.com>
2023-06-03 23:06:26 +0300

    Add pagination to children.html page

--- 3. API development with Flask/Lesson_14.3/homework/project/app.py
+++ 3. API development with Flask/Lesson_14.3/homework/project/app.py
@@ -1,20 +1,31 @@
 from flask import Flask, request, render_template
 from utils import load_needed_movie, load_children_movies_list
+from flask_paginate import Pagination, get_page_args
 
 
 app = Flask(__name__)
 
+
+# generating data for pagination
+users = list(range(100))
+
+def get_users(offset=0, per_page=10):
+    return users[offset: offset+per_page]
+
+
 @app.route("/movie/<title>/")
 def movie_page(title):
     context = load_needed_movie(title)
-    print(context)
     return render_template("movie.html", context=context)
 
 @app.route("/rating/children/")
 def children_rating_page():
     context = load_children_movies_list()
-    print(context)
-    return render_template("children.html", context=context)
+    page, per_page, offset = get_page_args(page_parameter="page", per_page_parameter="per_page")
+    total = len(users)
+    pagination_users = get_users(offset=offset, per_page=per_page)
+    pagination = Pagination(page=page, per_page=per_page, total=total, css_framework="bootstrap4")
+    return render_template("children.html", context=context, users=pagination_users, page=page, per_page=per_page, pagination=pagination)
 
 
 app.run(debug=True)
